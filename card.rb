4a97cd6337386312a3c7f19af0c7257f759be223
playrix <web.playrix@yandex.ru>
2017-07-14 16:21:05 +0300

    Add class card.rb

---
+++ card.rb
@@ -0,0 +1,17 @@
+class Card
+
+  def initialize(lear, face)
+    @lear = lear
+    @face = face
+  end
+
+  def value
+    return @face if @face.is_a?(Integer)
+    @face.eql?("A") ? 11 : 10
+  end
+
+  def show
+    puts "#{@face} #{@lear}"
+  end
+
+end
