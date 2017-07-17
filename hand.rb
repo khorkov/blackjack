5cdf30d15699b6fb1a6d0c6064bc9d9e1e0de508
playrix <web.playrix@yandex.ru>
2017-07-17 16:25:20 +0300

    Add class hand

--- hand.rb
+++ hand.rb
@@ -1,3 +1,29 @@
 class Hand
-  #
+
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+  end
+
+  def value
+    total = 0
+    ace_count = 0
+
+    @cards.each do |card|
+      ace_count += 1 if card.value == 11
+      total += card.value
+    end
+
+    ace_count >= 1 && total > 21 ? total - 10 : total
+  end
+
+  def display
+    @cards.each { |card| puts "#{card.display}" }
+  end
+
+  def display_hidden
+    @cards.count.times { puts "🂠 "  }
+  end
+
 end
